6ae9bf818d3a85f39664d45a7825a8d84bdb49e9
PrestonWinstead <prestonwinstead@me.com>
2018-01-01 10:32:13 -0600

    [deployment] update ports for deployment env

--- src/components/app/auth/auth-config.ts
+++ src/components/app/auth/auth-config.ts
@@ -9,7 +9,7 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   CLIENT_ID: 'FkcsFLCal4iwRdteOG4XUAlKOxXl0ojd',
   CLIENT_DOMAIN: 'findo.auth0.com', 
-  AUDIENCE: 'http://localhost:9000',
+  AUDIENCE: 'http://localhost:80',
   REDIRECT: 'http://localhost:8080/callback',
   SCOPE: 'openid profile email'
 };
